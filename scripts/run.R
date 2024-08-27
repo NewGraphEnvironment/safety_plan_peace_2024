292f992d4e268a9a271011586230bab91f0a5d3b
almac2022 <al@newgraphenvironment.com>
2024-08-27 11:12:50 -0700

    close #4

--- scripts/run.R
+++ scripts/run.R
@@ -3,18 +3,7 @@
 staticimports::import()
 
 {
-  rmarkdown::render(input = 'safety_plan.Rmd', output_file = 'docs/safety_plan.html')
-  pagedown::chrome_print(input = 'safety_plan.Rmd', output = 'docs/safety_plan.pdf')
+  rmarkdown::render(input = 'safety_plan.Rmd', output_file = 'docs/index.html')
+  pagedown::chrome_print(input = 'safety_plan.Rmd', output = 'docs/safety_plan_sern-peace-fish-passage.pdf')
 
-  # we want to seperately attach a map and a list of sites for DFO
-  #
-  ## trim up the file.  We ditch the last page only when there are references.
-
-
-  pdftools::pdf_subset(paste0(getwd(), "/docs/application_moe_dfo.pdf"),
-                       pages = 4, output = paste0(getwd(), "/docs/application_dfo_map.pdf"))
-
-  # trim up the file.  We ditch the last page only when there are references.
-  pdftools::pdf_subset(paste0(getwd(), "/docs/application_moe_dfo.pdf"),
-                       pages = 5:8, output = paste0(getwd(), "/docs/application_dfo_site_list.pdf"))
 }
