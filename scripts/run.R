99f4e5cd8a781b3a4b8e45a3e759c3eb45030684
almac2022 <al@newgraphenvironment.com>
2024-08-29 12:10:59 -0700

    rebuild with updated contacts

--- scripts/run.R
+++ scripts/run.R
@@ -3,7 +3,7 @@
 staticimports::import()
 
 
-# getting a buid failyre with chrome print so need to create a dummy favicon.ico file in the main directory (`touch favicon.ico` in terminal)
+###!!!!!!!!!!!!!!!!! was getting a buid failyre with chrome print so need to create a dummy favicon.ico file in the main directory (`touch favicon.ico` in terminal)
 {
   rmarkdown::render(input = 'safety_plan.Rmd', output_file = 'docs/index.html')
   pagedown::chrome_print(input = 'safety_plan.Rmd', output = 'docs/safety_plan_sern-peace-fish-passage.pdf')
